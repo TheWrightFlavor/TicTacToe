21f933d16f9a942283d8b5d0e26d01a47c4500a7
Daniel Wright <36201488+TheWrightFlavor@users.noreply.github.com>
2018-02-17 14:50:37 -0600

    Add files via upload

---
+++ src/Game/ShowButtons.java
@@ -0,0 +1,32 @@
+package Game;
+
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ShowButtons extends JButton implements ActionListener{
+	ImageIcon X;
+	ImageIcon O;
+	byte value=0;
+	
+	public ShowButtons() {
+		X=new ImageIcon(this.getClass().getResource("x.png"));
+		O=new ImageIcon(this.getClass().getResource("o.png"));
+		this.addActionListener(this);
+	}
+	public void actionPerformed(ActionEvent e) {
+		value++;
+		value%=3;
+		switch(value) {
+		case 0:
+			setIcon(null);
+			break;
+		case 1:
+			setIcon(X);
+			break;
+		case 2:
+			setIcon(O);
+		}
+	}
+}
---
+++ src/Game/TicTacToe.java
@@ -0,0 +1,31 @@
+package Game;
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+
+public class TicTacToe extends JFrame {
+	JPanel p=new JPanel();
+	ShowButtons buttons[]=new ShowButtons[9];
+	
+	public static void main(String args[]) {
+		new TicTacToe();	
+	}
+	
+	public TicTacToe() {
+		super("TicTacToe");
+		setSize(400,400);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		p.setLayout(new GridLayout(3,3));
+		for(int i=0; i<9; i++) {
+			buttons[i]=new ShowButtons();
+			p.add(buttons[i]);
+		}
+		add(p);
+		
+		setVisible(true);
+	}
+
+	
+}
