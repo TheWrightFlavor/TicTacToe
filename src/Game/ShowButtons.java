ede15fa9030f3cd7d6e8e87308a0b85d3ed4d545
Daniel Wright <36201488+TheWrightFlavor@users.noreply.github.com>
2018-02-18 23:24:26 -0600

    Update ShowButtons.java

--- src/Game/ShowButtons.java
+++ src/Game/ShowButtons.java
@@ -6,18 +6,24 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class ShowButtons extends JButton implements ActionListener{
+	/*
+	The Icons for the program are .png ; make sure they are in the same directory as your .class files.
+	*/
 	ImageIcon X;
 	ImageIcon O;
 	byte value=0;
 	
 	public ShowButtons() {
+		//Grabbing images for icons.
 		X=new ImageIcon(this.getClass().getResource("x.png"));
 		O=new ImageIcon(this.getClass().getResource("o.png"));
+		//Adding the action to be performed on button press.
 		this.addActionListener(this);
 	}
 	public void actionPerformed(ActionEvent e) {
 		value++;
 		value%=3;
+		
 		switch(value) {
 		case 0:
 			setIcon(null);
