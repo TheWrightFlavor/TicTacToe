6013e18b92859653e45e9e78733c78c4b020fd95
Daniel Wright <36201488+TheWrightFlavor@users.noreply.github.com>
2018-02-18 22:34:22 -0600

    Comments
    Added comments to show what is going on.

--- src/Game/TicTacToe.java
+++ src/Game/TicTacToe.java
@@ -13,15 +13,18 @@ public class TicTacToe extends JFrame {
 	}
 	
 	public TicTacToe() {
+		//Setting up the JPanel
 		super("TicTacToe");
 		setSize(400,400);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		p.setLayout(new GridLayout(3,3));
+		//Adding the Buttons from showButtons to the panel
 		for(int i=0; i<9; i++) {
 			buttons[i]=new ShowButtons();
 			p.add(buttons[i]);
 		}
+		//Adding the panel to the JFrame
 		add(p);
 		
 		setVisible(true);
